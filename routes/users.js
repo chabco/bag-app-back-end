c916236ab58545798e494f4a1a9072c0e1bf0887
charlescortez <charlesmph@gmail.com>
2019-10-06 16:53:30 -0400

    enable working sign up with token and hasAgreed

--- routes/users.js
+++ routes/users.js
@@ -32,45 +32,45 @@ router.get('/:id', (req, res) => {
 // Filter access to database.
 // Let front-end talk to DB, let a user sign up.
 router.post('/', (req, res) => {
-	const { username, email, password } = req.body;
-	if ((!username) || (!email) || (!password)) {
-		console.log(req.body)
-		// Empty entries? Stop.
-		res.json({
-			msg: "invalidData"
-		})
-		return;
-	}
-	const checkUserQuery = `SELECT * FROM users WHERE email = ?`;
-	db.query(checkUserQuery, [email], (err, results) => {
-		if (err) {throw err};
-		if (results.length > 0) {
-			res.json({
-				msg: "userExists"
-			})
-		}
-		else {
-			const insertUserQuery = `INSERT INTO users (username, email, password, token) VALUES (?, ?, ?, ?)`;
+	console.log(req.body)
+	const { username, email, password, hasAgreed } = req.body;
+	// if ((!username) || (!email) || (!password)) {
+	// 	// Empty entries? Stop.
+	// 	res.json({
+	// 		msg: "invalidData"
+	// 	})
+	// 	return;
+	// }
+	// const checkUserQuery = `SELECT * FROM users WHERE email = ?`;
+	// db.query(checkUserQuery, [email], (err, results) => {
+		// if (err) {throw err};
+		// if (results.length > 0) {
+		// 	res.json({
+		// 		msg: "userExists"
+		// 	})
+		// }
+		// else {
+			const insertUserQuery = `INSERT INTO users (username, email, password, hasAgreed, token) VALUES (?, ?, ?, ?, ?)`;
 
 			// Token and encrypted password. Safety first.
-			const salt = bcrypt.getSaltSync(10);
-			const hash = bcrypt.hashSync(password, salt);
+			// const salt = bcrypt.getSaltSync(10);
+			// const hash = bcrypt.hashSync(password, salt);
 			const token = randToken.uid(50);
 
-			db.query(insertUserQuery, [username, email, hash, token], (err) => {
+			db.query(insertUserQuery, [username, email, password, hasAgreed, token], (err) => {
 				if(err) {throw err};
 				// Successful entry.
 
 				res.json({
 					username,
 					email,
-					hash,
+					hasAgreed,
 					token,
 					msg: "userAdded"
 				})
 			})
-		}
-	})
+		// }
+	// })
 })
 
 // router.post('/'), ()
