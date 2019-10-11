36316e8cde99d8895627f996c3a7f2e465099efe
charlescortez <charlesmph@gmail.com>
2019-10-11 14:48:04 -0400

    deploy with front-end build in views: test

---
+++ views/precache-manifest.cad5c914d2fca5f70b22944bbf3e2724.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98d49e7be17cc7511156ed41ad9efeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdcac4aed3fbbc73398a",
+    "url": "/static/css/main.06b68011.chunk.css"
+  },
+  {
+    "revision": "f1f0558aeb3b6018ad51",
+    "url": "/static/js/2.381fb34a.chunk.js"
+  },
+  {
+    "revision": "cdcac4aed3fbbc73398a",
+    "url": "/static/js/main.037fa544.chunk.js"
+  },
+  {
+    "revision": "74026710aed7cbb810aa",
+    "url": "/static/js/runtime-main.2c09c760.js"
+  }
+]);
